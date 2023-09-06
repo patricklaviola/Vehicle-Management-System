9a79a1206dda619e303894b8ef5e80424d75eea9
Patrick La Viola <patrickjlaviola@gmail.com>
2023-09-05 20:06:50 -0700

    Created models, configured URLS

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.vin
+
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    employee_id = models.models.models.PositiveSmallIntegerField(unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Appointment(models.Model):
+    PENDING = 'PENDING'
+    COMPLETED = 'COMPLETED'
+    CANCELLED = 'CANCELLED'
+
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (COMPLETED, 'Completed'),
+        (CANCELLED, 'Cancelled'),
+    ]
+
+    date_time = models.DateTimeField()
+    reason = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING)
+    vin = models.CharField(max_length=17)
+    customer = models.CharField(max_length=100)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="Appointments",
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"Appointment for {self.customer} on {self.date_time}"
