f02f29b5e827e6d6831d7232f44d517343ac914a
Patrick La Viola <patrickjlaviola@gmail.com>
2023-09-10 16:39:43 -0700

    app.js uncommented

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,24 +2,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 
-// import ManufacturersList from './Manufacturer/ManufacturersList';
-// import ManufacturerForm from './Manufacturer/ManufacturerForm';
+import ManufacturersList from './Manufacturer/ManufacturersList';
+import ManufacturerForm from './Manufacturer/ManufacturerForm';
 
-// import ModelsList from './VehicleModels/ModelsList';
-// import ModelForm from './VehicleModels/ModelForm';
+import ModelsList from './VehicleModels/ModelsList';
+import ModelForm from './VehicleModels/ModelForm';
 
-// import AutomobilesList from './Automobile/AutomobilesList';
-// import AutomobileForm from './Automobile/AutomobileForm';
+import AutomobilesList from './Automobile/AutomobilesList';
+import AutomobileForm from './Automobile/AutomobileForm';
 
-// import SalespeopleList from './Salesperson/SalespeopleList';
-// import SalespersonForm from './Salesperson/SalespersonForm';
+import SalespeopleList from './Salesperson/SalespeopleList';
+import SalespersonForm from './Salesperson/SalespersonForm';
 
-// import CustomersList from './Customer/CustomersList';
-// import CustomerForm from './Customer/CustomerForm';
+import CustomersList from './Customer/CustomersList';
+import CustomerForm from './Customer/CustomerForm';
 
-// import SalesList from './Sale/SalesList';
-// import SaleForm from './Sale/SaleForm';
-// import SalespersonHistory from './Salesperson/SalespersonHistory';
+import SalesList from './Sale/SalesList';
+import SaleForm from './Sale/SaleForm';
+import SalespersonHistory from './Salesperson/SalespersonHistory';
 
 import TechniciansList from './Technician/TechniciansList';
 import TechnicianForm from './Technician/TechnicianForm';
@@ -38,24 +38,24 @@ function App() {
 
           <Route path="/" element={<MainPage />} />
 
-          {/* <Route path="/manufacturers" element={<ManufacturersList />} /> */}
-          {/* <Route path="/manufacturers/new" element={<ManufacturerForm />} /> */}
+          <Route path="/manufacturers" element={<ManufacturersList />} />
+          <Route path="/manufacturers/new" element={<ManufacturerForm />} />
 
-          {/* <Route path="/models" element={<ModelsList />} /> */}
-          {/* <Route path="/models/new" element={<ModelForm />} /> */}
+          <Route path="/models" element={<ModelsList />} />
+          <Route path="/models/new" element={<ModelForm />} />
     
-          {/* <Route path="/automobiles" element={<AutomobilesList />} /> */}
-          {/* <Route path="/automobiles/new" element={<AutomobileForm />} /> */}
+          <Route path="/automobiles" element={<AutomobilesList />} />
+          <Route path="/automobiles/new" element={<AutomobileForm />} />
 
-          {/* <Route path="/salespeople" element={<SalespeopleList />} /> */}
-          {/* <Route path="/salespeople/new" element={<SalespersonForm />} /> */}
+          <Route path="/salespeople" element={<SalespeopleList />} />
+          <Route path="/salespeople/new" element={<SalespersonForm />} />
 
-          {/* <Route path="/customers" element={<CustomersList />} /> */}
-          {/* <Route path="/customers/new" element={<CustomerForm />} /> */}
+          <Route path="/customers" element={<CustomersList />} />
+          <Route path="/customers/new" element={<CustomerForm />} />
 
-          {/* <Route path="/sales" element={<SalesList />} /> */}
-          {/* <Route path="/sales/new" element={<SaleForm />} /> */}
-          {/* <Route path="/sales/history" element={<SalespersonHistory />} /> */}
+          <Route path="/sales" element={<SalesList />} />
+          <Route path="/sales/new" element={<SaleForm />} />
+          <Route path="/sales/history" element={<SalespersonHistory />} />
           
           <Route path="/technicians" element={<TechniciansList />} />
           <Route path="/technicians/new" element={<TechnicianForm />} />
