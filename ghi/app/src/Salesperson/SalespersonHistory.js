c718e92ce3c6bea4fb33982788bc378808773386
Anna <akthorndike@gmail.com>
2023-09-11 17:55:18 -0700

    struggling with salespersonhistory part 2

--- ghi/app/src/Salesperson/SalespersonHistory.js
+++ ghi/app/src/Salesperson/SalespersonHistory.js
@@ -1,66 +1,89 @@
 import { useEffect, useState } from 'react';
 
 function SalespersonHistory() {
-  const [sales, setSales] = useState([])
-  
-  const getData = async () => {
-    const response = await fetch('http://localhost:8090/api/sales/');
-    
-    if (response.ok) {
-      const data = await response.json();
-      setSales(data.sales)
-    }
-  };
-  
-  useEffect(()=>{
-    getData()
-  }, [])
-  
-  
-  // FILTER BY SALESPERSON
-  const [searchBySalesperson, setSearchBySalesperson] = useState('');
-  const filteredSales = sales.filter(sale => {
-    return searchBySalesperson === '' || sale.salesperson.includes(searchBySalesperson);
-  });
-  const handleSearchChange = (e) => {
-    setSearchBySalesperson(e.target.value);
-  }
-  const handleClick = () => {
-    getData();
-  };
+    const [sales, setSales] = useState([])
+    const [filteredSales, setFilteredSales] = useState([])
+    const [salespeople, setSalespeople] = useState([]);
+    const [searchBySalesperson, setSearchBySalesperson] = useState('');
 
-  
-  return (
-    <>
-    <h1 className='py-4'>Service History</h1>
-    <div className="input-group mb-3">
-        <input value={searchBySalesperson} onChange={handleSearchChange} type="text" className="form-control" placeholder="Search by Salesperson..." aria-label="Search by Salesperson..." aria-describedby="button-addon2"/>
-        <button onClick={handleClick} className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
-    </div>
-    <table className="table table-striped">
-        <thead>
-            <tr>
-                <th>Salesperson</th>
-                <th>Customer</th>
-                <th>VIN</th>
-                <th>Price</th>
-            </tr>
-        </thead>
-        <tbody>
-        {filteredSales.map(sale => {
-            return (
-                <tr key={sale.id}>
-                    <td>{sale.salesperson.first_name} {sale.salesperson.last_name}</td>
-                    <td>{sale.customer.first_name} {sale.customer.last_name}</td>
-                    <td>{sale.automobile.vin}</td>
-                    <td>{sale.price}</td>
+    const fetchSalespeople = async () => {
+        const url = "http://localhost:8090/api/salespeople/";
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            setSalespeople(data.salespeople);
+        }
+    };
+
+    useEffect(() => {
+        fetchSalespeople()
+          .catch(err => {
+            console.log(err)
+          })
+    }, [])
+
+    const fetchSales = async () => {
+        setSearchBySalesperson('');
+        try {
+            const response = await fetch('http://localhost:8090/api/sales/');
+            const data = await response.json();      
+            setSales(data.sales);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    
+    useEffect(() => {
+        const filtered = sales.filter(sale => 
+          sale.salesperson.id === searchBySalesperson);  
+        console.log(filtered)  
+        setFilteredSales(filtered);    
+      }, [sales, searchBySalesperson]);
+    
+
+    const handleSearchChange = (e) => {
+        setSearchBySalesperson(e.target.value);
+        fetchSales(); 
+    }
+
+    return (
+        <>
+        <h1 className='py-4'>Service History</h1>
+        <label htmlFor="salesperson">Salesperson</label>
+        <div className="input-group mb-3">
+            <select onChange={handleSearchChange} value={searchBySalesperson} required name="salesperson" id="salesperson" className="form-select">
+                <option value="">Choose a Salesperson...</option>
+                {salespeople.map(salesperson => {
+                    return(
+                        <option key={salesperson.id} value={salesperson.id}>{salesperson.first_name} {salesperson.last_name}</option>
+                    )
+                })}
+            </select>
+        </div>
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th>Salesperson</th>
+                    <th>Customer</th>
+                    <th>VIN</th>
+                    <th>Price</th>
                 </tr>
-          );
-        })}
-      </tbody>
-    </table>
-    </>
-  );
+            </thead>
+            <tbody>
+            {sales.filter((sale) => sale.salesperson.id ).map((sale) => {
+                return (
+                    <tr key={sale.id}>
+                        <td>{sale.salesperson.first_name} {sale.salesperson.last_name}</td>
+                        <td>{sale.customer.first_name} {sale.customer.last_name}</td>
+                        <td>{sale.automobile.vin}</td>
+                        <td>{sale.price}</td>
+                    </tr>
+                );
+            })}
+        </tbody>
+        </table>
+        </>
+    );
 }
 
 export default SalespersonHistory;
